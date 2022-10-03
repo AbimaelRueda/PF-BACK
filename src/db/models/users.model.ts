cd58a3671cdf6dd01332033579d890f14ea13931
Abimael Rueda <aries_abimael@hotmail.com>
2022-10-03 18:02:29 -0500

    commit funcionando

---
+++ src/db/models/users.model.ts
@@ -0,0 +1,68 @@
+import { json } from "body-parser";
+import { DataTypes, Model } from "sequelize";
+import sequelizeConnection from "../db";
+
+interface UsersAttributes {
+  id?: number;
+  name: string;
+  lastname: string;
+  nickname: string;
+  date?: string;
+  picture: string;
+  email: string;
+  status: boolean;
+  category: string;
+}
+
+class users extends Model<UsersAttributes> {
+  public id!: number;
+  public name!: string;
+  public lastname!: string;
+  public nickname!: string;
+  public date!: string
+  public picture!: string;
+  public email!: string;
+  public status!: boolean;         
+  public category!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+  public readonly deletedAt!: Date;
+}
+
+users.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+    },
+    nickname: {
+      type: DataTypes.STRING,
+    },
+    picture: {
+      type: DataTypes.STRING,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    date: {
+      type: DataTypes.STRING,
+    },
+    status: {
+      type: DataTypes.BOOLEAN,
+    },
+    category: {
+      type: DataTypes.STRING,
+    },
+  },
+  { sequelize: sequelizeConnection, paranoid: true }
+);
+
+export default users;
